da99e8460bd77ed92d6a8b3671d787d69df4d62c
parjani <140376906+parjani@users.noreply.github.com>
2026-05-12 22:51:00 +0530

    Added Header Component

--- todolistapp/src/components/Header.jsx
+++ todolistapp/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header(){
     return(
         <>
-            <header className="todo-header">
+             <header className="todo-header">
                 <h1 className="todo-title">To-Do List</h1>
             </header>
         </>
